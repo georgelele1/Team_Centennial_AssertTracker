945737021f871dd550176a10f6f9ba6f79093de7
georgelele1 <43303561+georgelele1@users.noreply.github.com>
2019-12-11 16:57:04 -0500

    Initial commit

---
+++ app/src/main/java/com/example/asserttracking/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.example.asserttracking.Prevalent;
+
+import com.example.asserttracking.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
